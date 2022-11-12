9a852e02c48cf0d10689730b75e1d31121ab5405
Deanit <deanitz@gmail.com>
2022-11-12 20:16:55 +0300

    Компонент IngredientPropertyItem переведен на TypeScript

--- src/components/IngredientPropertyItem/IngredientPropertyItem.tsx
+++ src/components/IngredientPropertyItem/IngredientPropertyItem.tsx
@@ -1,18 +1,21 @@
-import PropTypes from "prop-types";
+import { FC } from "react";
 import styles from "./IngredientPropertyItem.module.css";
 
-const IngredientPropertyItem = ({ name, value }) => {
+export interface IIngredientPropertyItemProps {
+  name: string;
+  value: string;
+}
+
+const IngredientPropertyItem: FC<IIngredientPropertyItemProps> = ({
+  name,
+  value,
+}) => {
   return (
     <div className={styles.propertyContainer}>
       <p className="text text_type_main-default mb-1">{name}</p>
       <p className="text text_type_digits-default">{value}</p>
     </div>
   );
-};
-
-IngredientPropertyItem.propTypes = {
-  name: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired,
 };
 
 export default IngredientPropertyItem;
